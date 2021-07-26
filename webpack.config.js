1464651bc4a22df37246726f66c432d70ba79a71
LiuyiBest <1070692044@qq.com>
2021-07-26 10:59:16 +0800

    webpack

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+var path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        filename: '[name].[contenthash].js'
+    },
+    plugins: [new HtmlWebpackPlugin({
+        title: 'model',
+        template: 'src/assets/index.html'
+    }
+    )],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+        ],
+    }
+};
